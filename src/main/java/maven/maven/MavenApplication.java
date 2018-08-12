64ade37624de0f9cd379a5c219ee8591615e7285
bpo92007@gmail.com <bpo92007@gmail.com>
2018-08-12 13:09:03 -0400

    Setting spring proyect

---
+++ src/main/java/maven/maven/MavenApplication.java
@@ -0,0 +1,35 @@
+package maven.maven;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+@ComponentScan
+public class MavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+
+		};
+	}
+
+}
